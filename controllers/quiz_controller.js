caf0bea7ed8c58972b1c0ffc531f37160565031a
Eduardo Olarte <eolarte944@gmail.com>
2016-05-22 09:44:13 +0000

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -125,7 +125,16 @@ exports.update = function(req, res, next) {
     });
 };
 
-
+exports.destroy = function(req, res, next){
+	req.quiz.destroy()
+	  .then( function(){
+	  	req.flash('success', 'Quiz borrado con éxito');
+	  	res.redirect('/quizzes');
+	  })
+	  .catch(function(error){
+	  	req.flash('error', 'Error al editar el Quiz' +error.message);
+	  });
+};
 
 exports.show = function(req, res, next){
 	var answer = req.query.answer || '';
